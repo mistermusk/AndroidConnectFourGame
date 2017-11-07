943b1aeaa8c136cd465353045f99436188788ad6
Tingting Wang <twang80@ucsc.edu>
2017-11-06 23:26:41 -0800

    grid working

--- HW2/app/src/main/java/com/example/tingting/hw2/controller/GameViewFragment.java
+++ HW2/app/src/main/java/com/example/tingting/hw2/controller/GameViewFragment.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -22,13 +23,20 @@ import static android.content.ContentValues.TAG;
 
 public class GameViewFragment extends Fragment {
     GridView gridview;
+    GridView gridviewBlank;
     TextView tv;
+    // dummy string array to create grid view on touch
+    static String[] numbers;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.game_view_layout, container, false);
         tv = view.findViewById(R.id.textView);
         gridview = view.findViewById(R.id.gridView);
+        gridviewBlank = view.findViewById(R.id.gridViewBlank);
+        numbers = createNumGrid();
+
 
 
         gridview.setAdapter(new ImageAdapter(getActivity()));
@@ -44,19 +52,36 @@ public class GameViewFragment extends Fragment {
 
 
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                android.R.layout.simple_list_item_1, numbers);
 
-        view.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
+        gridviewBlank.setAdapter(adapter);
 
-                int x = (int)event.getX();
-                int y = (int)event.getY();
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_UP:
-                        Log.i("TAG", "location");
-                }
-                return false;
+        gridviewBlank.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "" + position, Toast.LENGTH_SHORT).show();
             }
         });
+
+
+
+//        gridView.setOnTouchListener(new View.OnTouchListener() {
+//            public boolean onTouch(View v, MotionEvent event) {
+//
+//                int x = (int)event.getX();
+//                int y = (int)event.getY();
+//                switch (event.getAction()) {
+//                    case MotionEvent.ACTION_UP:
+//                        Log.i("TAG", "location");
+//
+//                }
+//                return false;
+//            }
+//        });
+
+
 
 
         return view;
@@ -76,6 +101,18 @@ public class GameViewFragment extends Fragment {
             // get the cell[][] and current player info from text file
 
         }
+    }
+
+
+    public String[] createNumGrid()
+    {
+        String[] nums = new String[42];
+        for (int i = 0; i < 42; ++i)
+        {
+            nums[i] = "";
+        }
+
+        return nums;
     }
 
 
